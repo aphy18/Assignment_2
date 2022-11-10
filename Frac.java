1cb8ddcb5fac321e5104dc44b9fe6d9a71274f3d
aphy18 <aphason1@gmail.com>
2022-11-09 21:30:59 -0500

    calculating operations

--- Frac.java
+++ Frac.java
@@ -98,15 +98,6 @@ public class Frac implements ActionListener {
   }
 
   public void actionPerformed(ActionEvent e) {
-    String fracOneStr = fracOne.getText();
-    String fracTwoStr = fracTwo.getText();
-    String caseOperation = arithmeticSign.getText();
-
-    int num1 = Integer.parseInt(fracOneStr.substring(0,fracOneStr.indexOf('/')));
-    int denom1 = Integer.parseInt(fracOneStr.substring(fracOneStr.indexOf('/') + 1));
-    int num2 = Integer.parseInt(fracTwoStr.substring(0,fracTwoStr.indexOf('/')));
-    int denom2 = Integer.parseInt(fracTwoStr.substring(fracTwoStr.indexOf('/') + 1));
-    
     if (e.getSource() == quitItem) {
       System.exit(0);
     } else if (e.getSource() == helpItem) {
@@ -124,27 +115,43 @@ public class Frac implements ActionListener {
     } else if (e.getSource() == divideItem) {
       arithmeticSign.setText("/");
     } else if (e.getSource() == calculate) {
-        Operations newFrac = new Operations(num1, denom1);
+      try {
+        String fracOneStr = fracOne.getText();
+        String fracTwoStr = fracTwo.getText();
+        String caseOperation = arithmeticSign.getText();
+  
+        int num1 = Integer.parseInt(fracOneStr.substring(0,fracOneStr.indexOf('/')));
+        int denom1 = Integer.parseInt(fracOneStr.substring(fracOneStr.indexOf('/') + 1));
+        int num2 = Integer.parseInt(fracTwoStr.substring(0,fracTwoStr.indexOf('/')));
+        int denom2 = Integer.parseInt(fracTwoStr.substring(fracTwoStr.indexOf('/') + 1));
+
+        Operations firstFrac = new Operations(num1, denom1);
+
         switch (caseOperation) {
           case "+":
-          String add = newFrac.addRationals(num2, denom2);
-          calculation.setText(add);
+            String add = firstFrac.addRationals(num2, denom2);
+            calculation.setText(add);
           break;
           case "-":
-          String subtract = newFrac.subtractRationals(num2, denom2);
-          calculation.setText(subtract);
+            String subtract = firstFrac.subtractRationals(num2, denom2);
+            calculation.setText(subtract);
           break;
           case "x":
-          String multiply = newFrac.multiplyRationals(num2, denom2);
-          calculation.setText(multiply);
+            String multiply = firstFrac.multiplyRationals(num2, denom2);
+            calculation.setText(multiply);
+          break;
           case "/":
-          String divide = newFrac.divideRationals(num2, denom2);
-          calculation.setText(divide);
+            String divide = firstFrac.multiplyRationals(num2, denom2);
+            calculation.setText(divide);
           break;
           default:
-          System.out.println("operation doesn't exist");
+          System.out.println("Not a proper oepration");
           break;
         }
+
+      } catch (Exception err) {
+        System.out.println("Input not properly integrated");
+      }
     }
   }
 
